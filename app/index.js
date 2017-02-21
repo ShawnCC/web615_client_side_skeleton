76c30ce10106c51877e924ff3a2716b8e85ad08c
Shawn Crusvar <scrusvar@alterisgroup.net>
2017-02-20 19:24:30 -0500

    - Initial build

---
+++ app/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import React from 'react';
+
+export default class Index extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <p>Index Component</p>
+        );
+    }
+}
